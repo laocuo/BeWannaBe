23c56b70d5e27678dacefe88d47857629520e2e7
zhao_cheng1 <zhao_cheng1@hoperun.com>
2015-12-23 10:21:42 +0800

    Demo

---
+++ Demo/Demo_zhy_06_choujiangzhuanpan/gen/com/zhy/demo_zhy_06_choujiangzhuanpan/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.zhy.demo_zhy_06_choujiangzhuanpan;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
