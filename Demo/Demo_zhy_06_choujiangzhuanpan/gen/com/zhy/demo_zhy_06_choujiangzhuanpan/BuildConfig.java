cd4d014dc2e1d10179fb6a2da6ae96f73a65e72e
laocuo <laocuo@163.com>
2016-05-31 10:42:43 +0800

    delete others modules

--- Demo/Demo_zhy_06_choujiangzhuanpan/gen/com/zhy/demo_zhy_06_choujiangzhuanpan/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.zhy.demo_zhy_06_choujiangzhuanpan;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Demo/Demo_zhy_15_weixin_6_0_ui4/gen/com/zhy/weixin6/ui/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.zhy.weixin6.ui;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
